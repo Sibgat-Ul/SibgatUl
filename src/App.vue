f7665d5ddcd00e4905e81d48d680e5e2bfb41bb6
sibgatulislam@gmail.com <sibgatulislam@gmail.com>
2023-01-17 16:44:07 +0600

    fixed grid

--- src/App.vue
+++ src/App.vue
@@ -7,23 +7,32 @@ import Nav from '@/components/Nav.vue';
 <template>
   <div class="main h-screen p-.5em md:p-2em">
     <header>
-      <Nav class="mb-.5em md:mb-2em"/>
+      <Nav class="b-rd mb-.5em md:mb-2em"/>
     </header>
 
     <section 
       class="
         hero 
         h-[calc(100%-6em)] 
-        grid 
+        grid
         gap-1em
+        content-center
+        items-center
         md:gap-2em
-        
         grid-cols-1
         md:grid-cols-2
+        b-rd
       "
     >
-      <div class="">a</div>
-      <div class="">b</div>
+      <div class="p-1em">
+        <h2>
+          Lorem Ipsum
+        </h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores tempore impedit obcaecati, aspernatur inventore quod voluptatibus itaque quo blanditiis, dignissimos nihil natus non ipsa laborum soluta esse, officia eaque debitis!
+        </p>
+      </div>
+      <div class="p-1em ">b</div>
     </section>
   </div>
 </template>
@@ -31,7 +40,7 @@ import Nav from '@/components/Nav.vue';
 <style scoped>
 /* grid-cols-[repeat(auto-fit,minmax(20em,1fr))] */
 
-.hero > div {
+.hero  {
   background-color: var(--title-bg);
   color: var(--title-font);
 }
